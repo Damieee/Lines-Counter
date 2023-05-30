254b3ed8fd2fc62cc71151618e387dac76c60aad
Oluwadamilare EZEKIEL <oluwadamilare.ezekiel@kibo.school>
2023-05-30 08:19:26 +0100

    Refactor: Converted the code to a procedural approach from OOP

--- liine_counter.py
+++ liine_counter.py
@@ -1,32 +1,31 @@
-class LineCounter:
 
-    def __init__(self, filename: str):
-        self.filename = filename
+def count_lines(filename, lookup_value: str) -> int:
+    """
+    Counts the number of lines in the file.
+    If a lookup value is present, only counts lines that contain that value.
+    If the file does not exist, returns -1.
+    :param: lookup_value: string value to lookup for within the file
+    :return: int number of lines matching lookup value, or -1
+    """
+    # This error try
+    try:
+        with open(filename, 'r') as file:
+            lines = file.readlines()
+            if lookup_value:
+                lines = [line for line in lines if lookup_value in line]
+            return len(lines)
 
-    def count_lines(self, lookup_value: str) -> int:
-        """
-        Counts the number of lines in the file.
-        If a lookup value is present, only counts lines that contain that value.
-        If the file does not exist, returns -1.
-        :param: lookup_value: string value to lookup for within the file
-        :return: int number of lines matching lookup value, or -1
-        """
-        try:
-            with open(self.filename, 'r') as file:
-                lines = file.readlines()
-                if lookup_value:
-                    lines = [line for line in lines if lookup_value in line]
-                return len(lines)
-        except FileNotFoundError: 
-            return -1
+    # the except block handles the error when the file is not found
+    except FileNotFoundError: 
+        return -1
 
 
 # Usage example
 filename = "TopTechCompaniesInUsa.txt"
-counter = LineCounter(filename)
 lookup_value = "Software Engineering Internship"
-line_count = counter.count_lines(lookup_value)
-if line_count == -1:
+counter = count_lines(filename, lookup_value)
+
+if counter == -1:
     print("File not found.")
 else:
-    print(f"Number of lines that contains '{lookup_value}' are {line_count}")
+    print(f"Number of lines that contains '{lookup_value}' is {counter}")
